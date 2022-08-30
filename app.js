ff3707bcea730b8830935a6ae586e429fc72f5a8
Cerveux <cerveux@hotmail.com>
2022-08-30 10:39:48 -0300

    mejora de responsividad con js

--- app.js
+++ app.js
@@ -1,37 +1,59 @@
-var textarea = document.querySelector(".ingreso"); //text area de ingreso
-var alerta = document.querySelector(".alerta"); //texto que informa sobre may y acentos
-var btnEncriptar = document.querySelector(".encriptar"); //boton de encriptar
-var btnDesencriptar = document.querySelector(".desencriptar"); //boton de desencriptar
+const textarea = document.querySelector(".ingreso"); //text area de ingreso
+const alerta = document.querySelector(".alerta"); //texto que informa sobre may y acentos
+const btnEncriptar = document.querySelector(".encriptar"); //boton de encriptar
+const btnDesencriptar = document.querySelector(".desencriptar"); //boton de desencriptar
 
-var card = document.querySelector(".card");
-var cardInner = document.querySelector(".card-inner"); //inner card es lo que sirve para la funcionalidad de flip
-var textoResultado = document.querySelector(".texto-resultado"); //text area de salida
-var btnCopiar = document.querySelector(".copiar"); //boton de copiar
+const card = document.querySelector(".card");
+const cardInner = document.querySelector(".card-inner"); //inner card es lo que sirve para la funcionalidad de flip
+const textoResultado = document.querySelector(".texto-resultado"); //text area de salida
+const btnCopiar = document.querySelector(".copiar"); //boton de copiar
 
-var bugMozilla = document.querySelector(".card-title");
+const bugMozilla = document.querySelector(".card-title");
 
 var mensaje = "";
 var resultado;
 
-var comprobacion = true;
 const regEx = /[A-Zá-ú]/;
 
+var accion = false;
+
+var mobileCardHeight = 0;
+var desktopCardHeight = "775px";
+
+var alturaTexto = 0;
+
+function cardDesktop() {
+  card.style.height = desktopCardHeight;
+  textoResultado.style.height = "620px";
+}
+
+function cardMobile() {
+  textoResultado.style.height = "135px";
+  alturaTexto = textoResultado.scrollHeight + "px";
+  textoResultado.style.height = alturaTexto;
+  mobileCardHeight = textoResultado.scrollHeight + 160 + "px";
+  card.style.height = mobileCardHeight;
+}
+
 function cardAction() {
-  if(mensaje !== ""){
-    if((card.offsetHeight) !== 775){
-      textoResultado.style.height = ((textoResultado.scrollHeight)) + "px";
-      card.style.height = ((textoResultado.scrollHeight)+160) + "px";
-    }
+  if (mensaje !== "") {
+    textoResultado.style.height = "135px";
+    alturaTexto = textoResultado.scrollHeight + "px";
+    textoResultado.style.height =
+      card.offsetHeight !== 775 ? alturaTexto : "620px";
+    mobileCardHeight = textoResultado.scrollHeight + 160 + "px";
+(card.offsetHeight !== 775) && (card.style.height = mobileCardHeight);
+  
+
     cardInner.style.transform = "rotateY(180deg)";
     bugMozilla.style.display = "none";
-  };
+    accion = true;
+  }
 }
 
 function encriptar() {
   resultado = "";
   mensaje = textarea.value;
-
-  
 
   for (var i = 0; i < mensaje.length; i++) {
     switch (mensaje[i]) {
@@ -58,13 +80,12 @@ function encriptar() {
   }
   textoResultado.innerHTML = resultado;
   textarea.value = "";
-  comprobacion && cardAction();
+  cardAction();
 }
 
 function desencriptar() {
   resultado = "";
   mensaje = textarea.value;
-  
 
   for (var i = 0; i < mensaje.length; i++) {
     switch (mensaje[i]) {
@@ -96,7 +117,7 @@ function desencriptar() {
   }
   textoResultado.innerHTML = resultado;
   textarea.value = "";
-  comprobacion && cardAction();
+  cardAction();
 }
 
 function copiar() {
@@ -108,8 +129,19 @@ function agrandar() {
   setTimeout(() => alerta.classList.remove("accion"), 1000);
 }
 
+function reportWindowSize() {
+  if (accion) {
+    if (window.innerWidth > 912) {
+      cardDesktop();
+    } else {
+      cardMobile();
+    }
+  }
+}
+
 btnEncriptar.onclick = () =>
   regEx.test(textarea.value) ? agrandar() : encriptar();
 btnDesencriptar.onclick = () =>
   regEx.test(textarea.value) ? agrandar() : desencriptar();
 btnCopiar.onclick = copiar;
+window.onresize = reportWindowSize;
